e26672496ee1efdc61632c34aaa53b4367db3f5c
Jake Causier <jake@purplemountmedia.com>
2018-05-16 16:23:28 +0100

    Landing site

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <title>B2Ssystems</title>
     </head>
     <body>
         @yield('body')
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,5 +1,19 @@
 @extends('_layouts.master')
 
 @section('body')
-<h1>Hello world!</h1>
+
+<section id="content">
+
+    <div class="floating">
+
+        <h1><strong>B2systems</strong></h1>
+        <h4>We get the job done</h4>
+
+        <p>Our site is currently under construction at the moment.</p>
+        <p>Please check our social media channels below for more information or contact us.</p>
+
+    </div>
+
+</section>
+
 @endsection
